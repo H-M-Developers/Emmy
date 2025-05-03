fce1632691d501873775ddd8122f956dec5b3aac
CiprianoJr25 <hemersom_junior@outlook.com>
2025-05-02 22:55:32 -0400

    Emilayne_garcia 2.0

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function toggleMenu() {
+    const menuNav = document.getElementById('menu-nav');
+    menuNav.classList.toggle('active');
+}
+
+// Remove o preloader após o carregamento da página
+document.addEventListener("DOMContentLoaded", () => {
+    const preloader = document.getElementById("preloader");
+    const images = Array.from(document.images); // Obtém todas as imagens da página
+    let loadedImages = 0;
+
+    images.forEach((img) => {
+        if (img.complete) {
+            loadedImages++; // Incrementa se a imagem já está carregada
+        } else {
+            img.addEventListener("load", () => loadedImages++); // Incrementa ao carregar
+            img.addEventListener("error", () => loadedImages++); // Incrementa mesmo em caso de erro
+        }
+    });
+
+    const checkAllImagesLoaded = setInterval(() => {
+        if (loadedImages === images.length) {
+            clearInterval(checkAllImagesLoaded); // Para o intervalo quando todas as imagens forem carregadas
+            preloader.style.opacity = "0"; // Esconde o preloader com transição
+            setTimeout(() => {
+                preloader.style.display = "none"; // Remove o preloader da tela
+            }, 500); // Delay para a transição
+        }
+    }, 100); // Verifica a cada 100ms
+});
